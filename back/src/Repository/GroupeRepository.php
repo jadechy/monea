b9943964fcac4cfd6d4fdb123a903f151c016b21
Jade Chi-yen <chiyenjade@gmail.com>
2025-06-13 22:29:30 +0200

    feat: list of group by a user

--- back/src/Repository/GroupeRepository.php
+++ back/src/Repository/GroupeRepository.php
@@ -3,6 +3,7 @@
 namespace App\Repository;
 
 use App\Entity\Groupe;
+use App\Entity\User;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
@@ -14,6 +15,18 @@ class GroupeRepository extends ServiceEntityRepository
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Groupe::class);
+    }
+
+    public function findGroupesByUser(int $userId)
+    {
+        return $this->createQueryBuilder('g')
+            ->leftJoin('g.creator', 'c')
+            ->leftJoin('g.members', 'm')
+            ->leftJoin('m.individual', 'i')
+            ->where('c.id = :userId OR i.id = :userId')
+            ->setParameter('userId', $userId)
+            ->getQuery()
+            ->getResult();
     }
 
     //    /**
