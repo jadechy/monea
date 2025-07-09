f8951e2a7f27c9cdb462710e55f99342c7843df7
cecilelecerf <c.lecerf.cecile@gmail.com>
2025-07-09 21:08:07 +0200

    fix: refresh token

--- app/src/types/memberEnumType.ts
+++ app/src/types/memberEnumType.ts
@@ -1,4 +1,4 @@
 import { z } from "zod"
 
-export const MemberRoleEnum = z.enum(["author", "admin", "viewer", "member", "anonyme"])
+export const MemberRoleEnum = z.enum(["author", "admin", "viewer", "member", "anonyme", "banned"])
 export const MemberStatusEnum = z.enum(["pending", "accepted", "refused", "deleted"])
