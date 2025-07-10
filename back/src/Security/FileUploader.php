26da3e6d5e0f1af3013e51a9d5f42bf998a6eeb5
Jade Chi-yen <chiyenjade@gmail.com>
2025-07-10 12:16:37 +0200

    wip: add file upload service

---
+++ back/src/Security/FileUploader.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Service;
+
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\String\Slugger\SluggerInterface;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
+
+class FileUploader
+{
+    public function __construct(
+        #[Autowire('%kernel.project_dir%/public')] private string $targetDirectory,
+        private SluggerInterface $slugger,
+        private LoggerInterface  $logger,
+    ) {}
+
+    public function upload(UploadedFile $file, string $folder = '/img/uploads'): string
+    {
+        $originalFilename = pathinfo($file->getClientOriginalName(), PATHINFO_FILENAME);
+        $safeFilename = strtolower($this->slugger->slug($originalFilename));
+        $fileName = $safeFilename . '-' . uniqid() . '.' . $file->guessExtension();
+
+        try {
+            $file->move($this->targetDirectory . $folder, $fileName);
+        } catch (FileException $e) {
+            $this->logger->error('An error occurred while uploading the file: ' . $e->getMessage());
+        }
+
+        return $fileName;
+    }
+
+    public function getTargetDirectory(): string
+    {
+        return $this->targetDirectory;
+    }
+}
