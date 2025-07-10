56bdb50a559f42c49d4d218c8f82e4f3600158c9
Jade Chi-yen <chiyenjade@gmail.com>
2025-07-10 08:26:58 +0200

    feat: create member author when create groupe

--- back/src/DataFixtures/GroupeFixtures.php
+++ back/src/DataFixtures/GroupeFixtures.php
@@ -5,8 +5,11 @@ namespace App\DataFixtures;
 use App\Entity\Category;
 use App\Entity\Groupe;
 use App\Entity\User;
+use App\Entity\Member;
 use App\Enum\ColorEnum;
 use App\Enum\GroupTypeEnum;
+use App\Enum\MemberRoleEnum;
+use App\Enum\MemberStatusEnum;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Faker\Factory;
@@ -41,11 +44,22 @@ class GroupeFixtures extends Fixture implements DependentFixtureInterface
             /** @var User  */
             $user = $faker->randomElement($users);
             $manager->persist($groupe);
+
             $defaultCategory = new Category();
-            $defaultCategory->setLabel('default');
-            $defaultCategory->setColor(ColorEnum::Gray);
-            $defaultCategory->setGroupe($groupe);
+            $defaultCategory->setLabel('default')
+                ->setColor(ColorEnum::Gray)
+                ->setGroupe($groupe);
             $manager->persist($defaultCategory);
+
+            $member = new Member();
+            $member
+                ->setRole(MemberRoleEnum::AUTHOR)
+                ->setStatus(MemberStatusEnum::ACCEPTED)
+                ->setAddOn(new \DateTimeImmutable())
+                ->setGroupe($groupe)
+                ->setIndividual($user);
+            $manager->persist($member);
+
             $this->addReference('groupe_' . $i, $groupe);
         }
         $manager->flush();
