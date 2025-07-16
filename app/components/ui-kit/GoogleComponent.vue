478a4a84b455ad7497a4ad929edf9b147fb65deb
cecilelecerf <c.lecerf.cecile@gmail.com>
2025-07-16 14:20:39 +0200

    wip : url

--- app/components/ui-kit/GoogleComponent.vue
+++ app/components/ui-kit/GoogleComponent.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import logo from "../assets/google_logo.webp";
 const handleClick = () => {
-  window.location.href = "http://localhost:8000/connect/google";
+  window.location.href = "https://monea.life/connect/google";
 };
 </script>
 
@@ -10,5 +10,5 @@ const handleClick = () => {
     :src="logo"
     class="h-16 shadow rounded-xl w-fit py-4 px-6 hover:bg-gray-100 transition"
     @click="handleClick"
-  >
+  />
 </template>
