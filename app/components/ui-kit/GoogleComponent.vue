bde948b22322718e622324faa7d5dd9bda6ae289
cecilelecerf <c.lecerf.cecile@gmail.com>
2025-07-17 18:28:08 +0200

    wip : redirection /check

--- app/components/ui-kit/GoogleComponent.vue
+++ app/components/ui-kit/GoogleComponent.vue
@@ -1,14 +1,12 @@
 <script setup lang="ts">
 import logo from "../assets/google_logo.webp";
 const config = useRuntimeConfig();
-console.log(config.public.apiBase);
 const handleClick = () => {
   window.location.href = `${config.public.apiBase}/connect/google`;
 };
 </script>
 
 <template>
-  <div>API: {{ config.public.apiBase }}</div>
   <img
     :src="logo"
     class="h-16 shadow rounded-xl w-fit py-4 px-6 hover:bg-gray-100 transition"
